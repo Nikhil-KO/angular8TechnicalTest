e9e5b15cc7e42cb740ed3682f1a5e6eef47548c0
Nikhil <nikhiloommen12@gmail.com>
2020-12-14 00:04:38 +0000

    add come scaffolding

--- tech-test/src/app/components/to-do-list/to-do-list.component.ts
+++ tech-test/src/app/components/to-do-list/to-do-list.component.ts
@@ -13,13 +13,39 @@ export class ToDoListComponent implements OnInit {
   private _toDo: ToDo[];
   
   constructor(private _service : ToDoServiceService) { }
-
-  getList() : ToDo[] {
-    return this._toDo;
-  }
-
+  
   ngOnInit() {
     this._service.getList().subscribe(data => this._toDo = data);
   }
 
+  getList(): ToDo[] {
+    return this._toDo;
+  }
+
+  addTask(newTask: ToDo): void {
+    console.log(newTask);
+  }
+
+  // delete a completed task
+  deleteTask(id: number): void {
+    console.log(id);
+  }
+
+  // mark completed task as todo
+  undoTask(id: number): void {
+    console.log(id)
+  }
+
+  // update a key for given task id
+  updateTask(id: number, key: string, update: any): void {
+    console.log(id, key, update);
+  }
+
+  testFunction() {
+    console.log("ok");
+    console.log(this._toDo[0].hidden);
+    this._toDo[0].hidden = this._toDo[0].hidden ? false : true;
+    console.log(this._toDo[0].hidden);
+
+  }
 }
--- tech-test/src/app/interfaces/to-do.ts
+++ tech-test/src/app/interfaces/to-do.ts
@@ -4,4 +4,5 @@ export interface ToDo {
     description: string;
     category: string;
     done;
+    hidden?: boolean;
 }
